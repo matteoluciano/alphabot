fb799eaeaf0f5cd9bb3e7fc8ff01c7fad43abaac
Matteo Luciano <matteo.luciano07@gmail.com>
2026-01-07 16:57:21 +0100

    messo a posto il problema della pagina che ricaricava

--- wasd_app/app.py
+++ wasd_app/app.py
@@ -1,3 +1,4 @@
+# prima prova
 from flask import Flask, render_template, request
 
 # creare l'app
--- wasd_app/app_login.py
+++ wasd_app/app_login.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
+import sqlite3
 from AlphaBotV3 import AlphaBot
 
 Ab = AlphaBot()
