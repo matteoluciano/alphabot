45e504beed34052cbbc9ed46a9d91ca8b61d5cc8
TSLiam07 <157721427+l1ll14m@users.noreply.github.com>
2026-01-21 10:18:34 +0100

    Creazione app forme

---
+++ Forms_app/app.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from AlphaBotV3 import AlphaBot
+
+Ab = AlphaBot()
+Ab.stop()
+
+Ab.setPWMA(33) #S
+Ab.setPWMB(33) #D
+
+# creare l'app
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        cmd = request.form.get("cmd")
+
+        if cmd:
+            print("Comando ricevuto:", cmd)
+            if cmd == 'W':
+                print("Vado avanti")
+                Ab.forward(1)
+                print("Vado a sinistra")
+                Ab.left(0.28)
+            elif cmd == 'S':
+                print("Vado indietro")
+                Ab.backward(1)
+            elif cmd == 'D':
+                print("Vado a destra")
+                Ab.right(0.28)
+        return redirect(url_for("index"))
+
+    return render_template("index.html")
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=False, use_reloader=False)
