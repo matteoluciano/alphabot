112042df046e74664548ead9ba2441173060177c
TSLiam07 <157721427+l1ll14m@users.noreply.github.com>
2026-01-21 10:03:08 +0100

    niente

--- wasd_app/app_2.py
+++ wasd_app/app_2.py
@@ -20,7 +20,6 @@ def index():
             if cmd == 'W':
                 print("Vado avanti")
                 Ab.forward(1)
-            elif cmd == 'A':
                 print("Vado a sinistra")
                 Ab.left(0.28)
             elif cmd == 'S':
@@ -34,4 +33,4 @@ def index():
     return render_template("index.html")
 
 if __name__ == '__main__':
-     app.run(host='0.0.0.0', port=5000, debug=False, use_reloader=False)
+    app.run(host='0.0.0.0', port=5000, debug=False, use_reloader=False)
