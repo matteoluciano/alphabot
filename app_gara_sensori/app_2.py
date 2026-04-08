1e282bdd1e22151ae4d72bd7c11a57c42b5b2715
Matteo Luciano <matteo.luciano07@gmail.com>
2026-04-08 10:21:11 +0200

    app gara aggiornata con stato sensori

---
+++ app_gara_sensori/app_2.py
@@ -0,0 +1,87 @@
+from flask import Flask, render_template, request, jsonify
+from AlphaBotV3 import AlphaBot
+from flasgger import Swagger
+import threading
+import RPi.GPIO as GPIO
+
+Ab = AlphaBot()
+Ab.stop()
+Ab.setPWMA(33)
+Ab.setPWMB(33)
+
+app = Flask(__name__)
+
+move_thread = None
+
+app.config["SWAGGER"] = {
+    'title': 'API sensori',
+    'uiversion': 3
+}
+
+swagger = Swagger(app)
+
+@app.route('/api/sensori', methods=['GET'])
+def leggi_sensori():
+    """
+    Lettura sensori AlphaBot
+    ---
+    responses:
+      200:
+        description: Stato sensori
+    """
+    left = Ab.left_sensor()
+    right = Ab.right_sensor()
+
+    return jsonify({
+        "sensore_sinistro": left,
+        "sensore_destro": right
+    })
+
+def run_command(cmd):
+    if cmd == 'START_W':
+        Ab.setPWMA(50)
+        Ab.setPWMB(50)
+        Ab.forward(9999)
+    elif cmd == 'START_S':
+        Ab.setPWMA(50)
+        Ab.setPWMB(50)
+        Ab.backward(9999)
+    elif cmd == 'START_A':
+        Ab.setPWMA(31)
+        Ab.setPWMB(31)
+        Ab.left(9999)
+    elif cmd == 'START_D':
+        Ab.setPWMA(31)
+        Ab.setPWMB(31)
+        Ab.right(9999)
+
+@app.route("/", methods=["GET"])
+def index():
+    return render_template("index.html")
+
+@app.route("/cmd", methods=["POST"])
+def cmd():
+    global move_thread
+
+    data = request.get_json()
+    command = data.get("cmd") if data else None
+
+    if not command:
+        return jsonify({"status": "no command"})
+
+    print("Comando ricevuto:", command)
+
+    if command == 'STOP':
+        Ab.stop()
+        print("Stop")
+    elif command.startswith('START_'):
+        # Ferma il movimento precedente
+        Ab.stop()
+        # Avvia il nuovo movimento in un thread separato
+        move_thread = threading.Thread(target=run_command, args=(command,), daemon=True)
+        move_thread.start()
+
+    return jsonify({"status": "ok"})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=False, use_reloader=False)
--- file_alphabot/api/sensori_api.py
+++ file_alphabot/api/sensori_api.py
@@ -28,5 +28,4 @@ def leggi_sensori():
     })
 
 if __name__ == '__main__':
-    print("API attiva su: http://127.0.0.1:5000/apidocs/")
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True, use_reloader=False)
