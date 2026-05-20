3af9e2f4ae383f70210200b5d0cdc5f2b91f1f79
TSLiam07 <sailiam.tu@itiscuneo.edu.it>
2026-05-20 10:40:12 +0200

    alphabot libreria spostata

---
+++ Thingsboard/AlphaBotV3.py
@@ -0,0 +1,135 @@
+# modifica a tutte le funzioni di movimento (a tutti va passato il tempo)
+import RPi.GPIO as GPIO
+import time
+
+class AlphaBot(object):
+	
+	def __init__(self,in1=12,in2=13,ena=6,in3=20,in4=21,enb=26, ir_left=19, ir_right=16):
+		self.IN1 = in1
+		self.IN2 = in2
+		self.IN3 = in3
+		self.IN4 = in4
+		self.ENA = ena
+		self.ENB = enb
+
+		# Sensori di prossimità
+		self.IR_LEFT = ir_left
+		self.IR_RIGHT = ir_right
+
+		GPIO.setmode(GPIO.BCM)
+		GPIO.setwarnings(False)
+		GPIO.setup(self.IN1,GPIO.OUT)
+		GPIO.setup(self.IN2,GPIO.OUT)
+		GPIO.setup(self.IN3,GPIO.OUT)
+		GPIO.setup(self.IN4,GPIO.OUT)
+		GPIO.setup(self.ENA,GPIO.OUT)
+		GPIO.setup(self.ENB,GPIO.OUT)
+		
+		GPIO.setup(self.IR_LEFT, GPIO.IN)
+		GPIO.setup(self.IR_RIGHT, GPIO.IN)
+
+
+		self.PWMA = GPIO.PWM(self.ENA,500)
+		self.PWMB = GPIO.PWM(self.ENB,500)
+		self.PWMA.start(50)
+		self.PWMB.start(50)
+
+	def forward(self, t):
+		GPIO.output(self.IN1,GPIO.HIGH)
+		GPIO.output(self.IN2,GPIO.LOW)
+		GPIO.output(self.IN3,GPIO.LOW)
+		GPIO.output(self.IN4,GPIO.HIGH)
+		
+		time.sleep(t)
+		self.stop()
+		time.sleep(1)
+
+	def stop(self):
+		GPIO.output(self.IN1,GPIO.LOW)
+		GPIO.output(self.IN2,GPIO.LOW)
+		GPIO.output(self.IN3,GPIO.LOW)
+		GPIO.output(self.IN4,GPIO.LOW)
+
+	def backward(self, t):
+		GPIO.output(self.IN1,GPIO.LOW)
+		GPIO.output(self.IN2,GPIO.HIGH)
+		GPIO.output(self.IN3,GPIO.HIGH)
+		GPIO.output(self.IN4,GPIO.LOW)
+
+		time.sleep(t)
+		self.stop()
+		time.sleep(1)
+
+
+	def left(self, t):
+		GPIO.output(self.IN1,GPIO.LOW)
+		GPIO.output(self.IN2,GPIO.HIGH)
+		GPIO.output(self.IN3,GPIO.LOW)
+		GPIO.output(self.IN4,GPIO.HIGH)
+
+		time.sleep(t)
+		self.stop()
+		time.sleep(1)
+
+
+	def right(self, t):
+		GPIO.output(self.IN1,GPIO.HIGH)
+		GPIO.output(self.IN2,GPIO.LOW)
+		GPIO.output(self.IN3,GPIO.HIGH)
+		GPIO.output(self.IN4,GPIO.LOW)
+
+		time.sleep(t)
+		self.stop()
+		time.sleep(1)
+		
+	def setPWMA(self,value):
+		self.PWMA.ChangeDutyCycle(value)
+
+	def setPWMB(self,value):
+		self.PWMB.ChangeDutyCycle(value)	
+		
+	def setMotor(self, left, right):
+		if((right >= 0) and (right <= 100)):
+			GPIO.output(self.IN1,GPIO.HIGH)
+			GPIO.output(self.IN2,GPIO.LOW)
+			self.PWMA.ChangeDutyCycle(right)
+		elif((right < 0) and (right >= -100)):
+			GPIO.output(self.IN1,GPIO.LOW)
+			GPIO.output(self.IN2,GPIO.HIGH)
+			self.PWMA.ChangeDutyCycle(0 - right)
+		if((left >= 0) and (left <= 100)):
+			GPIO.output(self.IN3,GPIO.HIGH)
+			GPIO.output(self.IN4,GPIO.LOW)
+			self.PWMB.ChangeDutyCycle(left)
+		elif((left < 0) and (left >= -100)):
+			GPIO.output(self.IN3,GPIO.LOW)
+			GPIO.output(self.IN4,GPIO.HIGH)
+			self.PWMB.ChangeDutyCycle(0 - left)
+	
+	# Ritorna 0 se trova l'ostacolo
+	def left_sensor(self):
+		return GPIO.input(self.IR_LEFT) == 0
+	
+	def right_sensor(self):
+		return GPIO.input(self.IR_RIGHT) == 0
+	
+	def cerchio(self):
+		for i in range(16):
+			self.forward(0.6)
+			self.left(0.2)
+	
+	def quadrato(self):
+		self.forward(2)
+		self.left(0.28)
+		self.forward(2)
+		self.left(0.28)
+		self.forward(2)
+		self.left(0.28)
+		self.forward(2)
+	
+	def triangolo(self):
+		self.forward(2)
+		self.left(0.42)
+		self.forward(2)
+		self.left(0.42)
+		self.forward(2)
